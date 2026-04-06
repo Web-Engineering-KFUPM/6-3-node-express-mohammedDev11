797b223df0d59a3ba68920973f568bf28a1b3ab1
John Doe <johndoe@email.com>
2026-04-06 10:18:46 +0300

    TODO

--- 6-3-node-express/backend/quotes.js
+++ 6-3-node-express/backend/quotes.js
@@ -1,12 +1,17 @@
 // Import random.js file
+import { getRandomInt } from "./utils/random.js";
 
 const quotes = [
   "Code is like humor. When you have to explain it, it’s bad.",
   "Before software can be reusable it first has to be usable.",
   "Make it work, make it right, make it fast.",
-  "Programming isn’t about what you know; it’s about what you can figure out."
+  "Programming isn’t about what you know; it’s about what you can figure out.",
 ];
 
 // TODO 3: Create and export getRandomQuote function
 //Hint: export function getRandomQuote(){}
 
+export function getRandomQuote() {
+  const index = getRandomInt(quotes.length);
+  return quotes[index];
+}
--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -1,18 +1,33 @@
 // TODO 1: Import and Create express app instance
 
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { getRandomQuote } from "./quotes.js";
+
+const app = express();
+
 // TODO 1: Define server port
-
-
+const PORT = 3000;
 // TODO 4: Import and Apply CORS middleware
-
+app.use(cors());
 
 // TODO 5: Import and apply Morgan middleware
-
+app.use(morgan("dev"));
 
 // TODO 6.1: Create root route "/"
-
+app.get("/", (req, res) => {
+  res.send("Backend server is running");
+});
 
 // TODO 6.2: Create "/api/quote" route
 
-
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 // TODO 7: Start server using app.listen
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
